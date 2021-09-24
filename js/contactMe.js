c62623dda313fcab9229fe98c6d3b1584dd379c4
Aidan Chao <achao@udel.edu>
2021-09-24 16:24:35 -0400

    Email form working, still need to check input format

---
+++ js/contactMe.js
@@ -0,0 +1,15 @@
+(function() {
+    // https://dashboard.emailjs.com/admin/integration
+    emailjs.init("user_bFRWWvQ1pmSB10ZlxSJWf");
+})();
+
+function sendMail() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailjs.sendForm("contact_service", "contact_form", this)
+        .then(function() {
+            console.log("SUCCESS");
+        });
+    });
+
+}
